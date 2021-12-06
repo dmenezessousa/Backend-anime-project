41d5191c2842d079c56555bcb428a5d86ec19cb2
KheSanh-Code <dmenezessousaa1@gmail.com>
2021-12-06 15:07:37 -0500

    setting up user router

--- routes/user/userRouter.js
+++ routes/user/userRouter.js
@@ -1,4 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const {
 
+} = require("");
+
+
+const {
+
+} = require("");
+
+
+router.post("/create-user",
+
+);
+
+router.post("/login",
+
+);
+
+router.put("/update-profile",
+
+);
+
+router.delete("/update-profile",
+
+);
 module.exports = router;
