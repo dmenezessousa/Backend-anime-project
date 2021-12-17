1478e41b024495febb65ea6daa4d24041c0955e3
KheSanh-Code <dmenezessousaa1@gmail.com>
2021-12-17 11:05:08 -0500

    fixed update user

--- routes/anime/model/Anime.js
+++ routes/anime/model/Anime.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 
-const animeSchema = new mongoose.Schema({
-    title:{type: String},
-    animePoster:{ type: String},
-    animeID:{type: String},
-    animeOwner:{type: mongoose.Schema.ObjectId, ref: "user"},
-},{timestamps : true,});
+const animeSchema = new mongoose.Schema(
+  {
+    title: { type: String },
+    animePoster: { type: String },
+    animeID: { type: String },
+    animeOwner: { type: mongoose.Schema.ObjectId, ref: "user" },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("anime", animeSchema);
--- routes/users/userRouter.js
+++ routes/users/userRouter.js
@@ -3,41 +3,42 @@ const router = express.Router();
 const passport = require("passport");
 
 const {
-    userSignUp,
-    userLogin,
-    updateUser
-} = require('./controller/Controller');
-
+  userSignUp,
+  userLogin,
+  updateUser,
+} = require("./controller/Controller");
 
 const {
-    checkIsEmpty,
-    checkIsUndefined,
-    validateCreateData,
-    validateLoginData,
-    validateUpdateData,
-} = require('../lib');
+  checkIsEmpty,
+  checkIsUndefined,
+  validateCreateData,
+  validateLoginData,
+  validateUpdateData,
+} = require("../lib");
 
-
-router.post("/create-user",
-    checkIsEmpty,
-    checkIsUndefined,
-    validateCreateData,
-    userSignUp,
+router.post(
+  "/create-user",
+  checkIsEmpty,
+  checkIsUndefined,
+  validateCreateData,
+  userSignUp
 );
 
-router.post("/login",
-    checkIsEmpty,
-    checkIsUndefined,
-    validateLoginData,
-    userLogin,
+router.post(
+  "/login",
+  checkIsEmpty,
+  checkIsUndefined,
+  validateLoginData,
+  userLogin
 );
 
-router.put("/update-profile",
-    passport.authenticate("jwt-user",{session:false}),
-    checkIsEmpty,
-    checkIsUndefined,
-    validateUpdateData,
-    updateUser,
+router.put(
+  "/update-profile",
+  passport.authenticate("jwt-user", { session: false }),
+  checkIsEmpty,
+  checkIsUndefined,
+  validateUpdateData,
+  updateUser
 );
 
 module.exports = router;
